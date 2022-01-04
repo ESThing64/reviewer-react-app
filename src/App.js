ca9b57c2ce627ed148680fb9606f5363523a1605
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2022-01-03 18:08:36 -0600

    added a feedback form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedBackData";
 import Card from "./components/shared/Card";
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -24,6 +25,7 @@ function App() {
 
       <Header />
        <div className="container">
+         <FeedbackForm />
        <FeedbackStats feedback={feedback} />
        <FeedbackList feedback={feedback}
        handleDelete={deleteFeedBack} />
